9ea1073e258b1cdb7530f0a237a4d6fbd583b99c
Mihaja PHIDIA <mihaja@phidia.onmicrosoft.com>
2023-06-12 18:05:59 +0300

    local changes

--- AmadeusDecoder/views/Tools.py
+++ AmadeusDecoder/views/Tools.py
@@ -19,7 +19,7 @@ def call_product_import(request):
     
     status = ''
     try:
-        directory = '/opt/odoo/issoufali-addons/sync_products/data/exported/'
+        directory = '/opt/odoo/issoufali-addons/export_contacts/data/exported/'
         for file in os.listdir(directory):
             if os.path.isfile(os.path.join(directory, file)):
                 ProductParser.import_product(os.path.join(directory, file), directory)
@@ -35,7 +35,7 @@ def call_customer_import(request):
     
     status = ''
     try:
-        directory = '/opt/odoo/issoufali-addons/sync_contacts/data/exported/'
+        directory = '/opt/odoo/issoufali-addons/export_contacts/data/exported/'
         for file in os.listdir(directory):
             if os.path.isfile(os.path.join(directory, file)):
                 CustomerParser.import_customer(os.path.join(directory, file), directory)

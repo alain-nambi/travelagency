f71349d1e7be80894952ae06f791c2d4dbe9eb8e
Ra Sam <handrynax@gmail.com>
2023-09-05 16:10:19 +0300

    data configuration insertion

--- AmadeusDecoder/sql_scripts/data/Issoufali/2023-07-25_SavingProtocols_Configs.sql
+++ AmadeusDecoder/sql_scripts/data/Issoufali/2023-07-25_SavingProtocols_Configs.sql
@@ -1,6 +1,18 @@
+-- If the environment is TEST --
+
 --If there is a line delete that before--
 delete from t_configuration where name = 'Saving File Tools';
 
 --Insert the new value of the configuartion--
 insert into t_configuration (environment, name, to_be_applied_on, value_name, single_value, dict_value, created_on, last_update, is_active) values 
-('all', 'Saving File Tools', 'Global', 'File protocol', 'Local', '"link"=>"testodoo.issoufali.phidia.fr/web/syncorders"', now(), now(), true);
+('test', 'Saving File Tools', 'Global', 'File protocol', 'Local', '"link"=>"https://testodoo.issoufali.phidia.fr/web/syncorders"', now(), now(), true);
+
+
+-- If the environment is PROD --
+
+--If there is a line delete that before--
+delete from t_configuration where name = 'Saving File Tools';
+
+--Insert the new value of the configuartion--
+insert into t_configuration (environment, name, to_be_applied_on, value_name, single_value, dict_value, created_on, last_update, is_active) values 
+('prod', 'Saving File Tools', 'Global', 'File protocol', 'Local', '"link"=>"https://odoo.issoufali.phidia.fr/web/syncorders"', now(), now(), true);
--- AmadeusDecoder/sql_scripts/data/SIGM/2023-07-25_SavingProtocols_Configs.sql
+++ AmadeusDecoder/sql_scripts/data/SIGM/2023-07-25_SavingProtocols_Configs.sql
@@ -1,6 +1,18 @@
+-- If the environment is TEST --
+
 --If there is a line delete that before--
 delete from t_configuration where name = 'Saving File Tools';
 
 --Insert the new value of the configuartion--
 insert into t_configuration (environment, name, to_be_applied_on, value_name, single_value, dict_value, created_on, last_update, is_active) values 
-('all', 'Saving File Tools', 'Global', 'File protocol', 'FTP', '"hostname"=>"51.75.181.181", "port"=>21, "username"=>"sigm", "password"=>"MGbi@261**", "repository"=>"/files/TEST", "link"=>"51.75.181.181/web/syncorders"', now(), now(), true);
+('test', 'Saving File Tools', 'Global', 'File protocol', 'FTP', '"hostname"=>"51.75.181.181", "port"=>21, "username"=>"sigm", "password"=>"MGbi@261**", "repository"=>"/files/TEST", "link"=>"51.75.181.181/web/syncorders"', now(), now(), true);
+
+
+-- If the environment is PROD --
+
+--If there is a line delete that before--
+delete from t_configuration where name = 'Saving File Tools';
+
+--Insert the new value of the configuartion--
+insert into t_configuration (environment, name, to_be_applied_on, value_name, single_value, dict_value, created_on, last_update, is_active) values 
+('prod', 'Saving File Tools', 'Global', 'File protocol', 'FTP', '"hostname"=>"51.75.181.181", "port"=>21, "username"=>"sigm", "password"=>"MGbi@261**", "repository"=>"/files/TEST", "link"=>"51.75.181.181/web/syncorders"', now(), now(), true);

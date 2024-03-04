d65b680139421098251104a5e7ad52a295350d34
alain-nambi <alainnambi@gmail.com>
2024-03-04 17:36:23 +0300

    fix: send fee reduce report everyday at 11:59pm o'clock

--- AmadeusDecoder/utilities/MailNotificationParser.py
+++ AmadeusDecoder/utilities/MailNotificationParser.py
@@ -681,17 +681,17 @@ class MailNotification():
     def fee_decrease_request(now):
         dt_now = now
         time_now = dt_now.time()
-        time_to_send = time(10, 20, 2)
+        time_to_send = time(23, 59, 59)
 
 
         #Liste des demandes de réduction de frais (ReducePnrFeeRequest)
         reduce_pnr_fee_request = ReducePnrFeeRequest.objects.filter(system_creation_date__date=dt_now).all()
-        print('------------------ EMAIL REDUCE FEE REQUEST ----------------------------')
-        print(time_now)
-        print(time_to_send)
-        for fee_request in reduce_pnr_fee_request :
-            print(fee_request.pnr.id )
-            print(fee_request.fee.id)
+        # print('------------------ EMAIL REDUCE FEE REQUEST ----------------------------')
+        # print(time_now)
+        # print(time_to_send)
+        # for fee_request in reduce_pnr_fee_request :
+        #     print(fee_request.pnr.id )
+        #     print(fee_request.fee.id)
 
         def pnr_line_data_for_reduce_fee_pnr():
             ISSOUFALI_URL = 'https://pnr.issoufali.phidia.fr'
@@ -742,12 +742,11 @@ class MailNotification():
         mgbi_users_mail = [
             "phpr974@gmail.com",
             "pp@phidia.onmicrosoft.com",
-            "nasolo@phidia.onmicrosoft.com",
             "tahina@phidia.onmicrosoft.com",
             "alain@phidia.onmicrosoft.com",
-            "anjaranaivo464@gmail.com",
             "olyviahasina.razakamanantsoa@outlook.fr",
-            "mathieu@phidia.onmicrosoft.com",
+            "maphieSarobidy@outlook.fr",
+            "alainnambi@gmail.com",
         ]
         
         other_users_mail = [
@@ -784,9 +783,10 @@ class MailNotification():
             "shoulaya@agences-issoufali.com",
         ]
         
-             
-        if time_now == time_to_send: # 9h00
-            print("C'EST L'HEURE D'ENVOYER L'EMAIL")
+        
+        if time_now == time_to_send: # 18:00
+            print("📢 Sending mail for fee decrease request...")
+            print("📢 C'EST L'HEURE D'ENVOYER L'EMAIL DE DIMINUTION DE FRAIS DE SERVICE")
             if len(reduce_pnr_fee_request) > 0:
                 subject = f"Demande de réduction de frais, ce {dt_now.strftime('%d-%m-%Y')}"                    
                 message = f"""        
@@ -805,9 +805,8 @@ class MailNotification():
                                     <th style="border:1px solid #ddd;padding:8px;padding-top:12px;padding-bottom:12px;text-align:left;background-color:#17a2b8;color:white;">Numéro du Billet</th>
                                     <th style="border:1px solid #ddd;padding:8px;padding-top:12px;padding-bottom:12px;text-align:left;background-color:#17a2b8;color:white;">Article</th>
                                     <th style="border:1px solid #ddd;padding:8px;padding-top:12px;padding-bottom:12px;text-align:left;background-color:#17a2b8;color:white;">Montant Original</th>
-                                    <th style="border:1px solid #ddd;padding:8px;padding-top:12px;padding-bottom:12px;text-align:left;background-color:#17a2b8;color:white;">Montant</th>
-                                    <th style="border:1px solid #ddd;padding:8px;padding-top:12px;padding-bottom:12px;text-align:left;background-color:#17a2b8;color:white;">Emetteur</th>
-
+                                    <th style="border:1px solid #ddd;padding:8px;padding-top:12px;padding-bottom:12px;text-align:left;background-color:#17a2b8;color:white;">Montant demandé</th>
+                                    <th style="border:1px solid #ddd;padding:8px;padding-top:12px;padding-bottom:12px;text-align:left;background-color:#17a2b8;color:white;">Agent demandeur</th>
                                 </tr>
                             </thead>
                             <tbody>
@@ -821,7 +820,8 @@ class MailNotification():
                 # Envoyer le mail pour les administrateurs d'Isssoufali 
                 Sending.send_email(
                     ANOMALY_EMAIL_SENDER["address"], 
-                    # administrator_users_mail + other_users_mail + mgbi_users_mail,
+                    # administrator_users_mail + mgbi_users_mail,
+                    ["alainnambi@gmail.com"],
                     subject, 
                     message
                 )
--- EmailFetcher/apps.py
+++ EmailFetcher/apps.py
@@ -119,6 +119,15 @@ def send_fee_update_list():
     # while True:
     #     schedule.run_pending()
     #     time.sleep(1)
+    
+# Fonction that send fee decrease report everyday at 6am o'clock
+def checking_pnr_with_fee_decrease_request():
+    from AmadeusDecoder.utilities.MailNotificationParser import MailNotification
+    now = datetime.now(timezone.utc).replace(microsecond=0) + timedelta(hours=3)
+    # print('---------------------------- COUCOU ------------------------------')
+    # print(now)
+    # ====================== PNR with fee decrease request ======================
+    MailNotification.fee_decrease_request(now)
 
 def fetch_email():
     try:
@@ -190,38 +199,42 @@ class EmailfetcherConfig(AppConfig):
         now = datetime.now()
         repeat_timer_for_pnr_upload_notification = 0
         
-        def pnr_upload_repeat_timer(repeat_timer_for_pnr_upload_notification):
-            print("📢 Mail notification for pnr not updated in pnr management...")
-            timer_update_check = RepeatTimer(repeat_timer_for_pnr_upload_notification, checking_pnr_not_uploaded_in_pnr_management)
-            timer_update_check.start()
+        # def pnr_upload_repeat_timer(repeat_timer_for_pnr_upload_notification):
+        #     print("📢 Mail notification for pnr not updated in pnr management...")
+        #     timer_update_check = RepeatTimer(repeat_timer_for_pnr_upload_notification, checking_pnr_not_uploaded_in_pnr_management)
+        #     timer_update_check.start()
     
-        if now.weekday() in [0, 1, 2, 3, 4]: # [Lundi, Mardi, Mercredi, Jeudi, Vendredi]            
-            repeat_timer_for_pnr_upload_notification = 10 * 60
-            pnr_upload_repeat_timer(repeat_timer_for_pnr_upload_notification)
-        if now.weekday() in [5]: # [Samedi]            
-            repeat_timer_for_pnr_upload_notification = 60 * 60
-            pnr_upload_repeat_timer(repeat_timer_for_pnr_upload_notification)
-        if now.weekday() in [6]: # [Dimanche]
-            repeat_timer_for_pnr_upload_notification = 60 * 180
-            pnr_upload_repeat_timer(repeat_timer_for_pnr_upload_notification)
+        # if now.weekday() in [0, 1, 2, 3, 4]: # [Lundi, Mardi, Mercredi, Jeudi, Vendredi]            
+        #     repeat_timer_for_pnr_upload_notification = 10 * 60
+        #     pnr_upload_repeat_timer(repeat_timer_for_pnr_upload_notification)
+        # if now.weekday() in [5]: # [Samedi]            
+        #     repeat_timer_for_pnr_upload_notification = 60 * 60
+        #     pnr_upload_repeat_timer(repeat_timer_for_pnr_upload_notification)
+        # if now.weekday() in [6]: # [Dimanche]
+        #     repeat_timer_for_pnr_upload_notification = 60 * 180
+        #     pnr_upload_repeat_timer(repeat_timer_for_pnr_upload_notification)
         
-        print("==================== Mail notification for pnr not sent to Odoo ====================")
-        timer_update_check = RepeatTimer(1, checking_pnr_not_sent_to_odoo)
-        timer_update_check.start()
+        # print("==================== Mail notification for pnr not sent to Odoo ====================")
+        # timer_update_check = RepeatTimer(1, checking_pnr_not_sent_to_odoo)
+        # timer_update_check.start()
         
-        print('Mail notification is starting....')
-        timer_pnr_misssing = RepeatTimer(1, checking_pnr_missing)
-        timer_pnr_misssing.start()
-        timer_passenger_segment_missing = RepeatTimer(1, checking_passenger_segment_missing)
-        timer_passenger_segment_missing.start()
+        # print('Mail notification is starting....')
+        # timer_pnr_misssing = RepeatTimer(1, checking_pnr_missing)
+        # timer_pnr_misssing.start()
+        # timer_passenger_segment_missing = RepeatTimer(1, checking_passenger_segment_missing)
+        # timer_passenger_segment_missing.start()
 
-        print('Daily Pnr created starting')
-        timer_schedule = RepeatTimer(60, start_pnr_daily_report_schedule)
-        timer_schedule.start()
+        # print('Daily Pnr created starting')
+        # timer_schedule = RepeatTimer(60, start_pnr_daily_report_schedule)
+        # timer_schedule.start()
 
-        print('Pnr unissued OPC checking is running...')
-        timer = RepeatTimer(60, pnr_unissued_opc_checking)  
-        timer.start()
+        # print('Pnr unissued OPC checking is running...')
+        # timer = RepeatTimer(60, pnr_unissued_opc_checking)  
+        # timer.start()
+        
+        # print("📢 ==================== Mail notification for pnr with fee decrease request ====================")
+        # timer_update_check = RepeatTimer(1, checking_pnr_with_fee_decrease_request)
+        # timer_update_check.start()
         
         # print('Product synchronisation is starting')
         # timer_synchro = RepeatTimer(5, running_product_synhcro)
@@ -231,6 +244,6 @@ class EmailfetcherConfig(AppConfig):
         # dest_dir = '/export/products'
         
         # send daily pnr fee update report
-        daily_thread_once = RepeatTimer(3600, send_fee_update_list)
-        daily_thread_once.start()
+        # daily_thread_once = RepeatTimer(3600, send_fee_update_list)
+        # daily_thread_once.start()
         

c6c29429d5bb1676cc3f6e5adc15ccbba190c42a
gitPhidia <github@phidia.fr>
2024-10-03 21:13:54 +0300

    inner_timeout

--- EmailFetcher/utilities/email_listener/__init__.py
+++ EmailFetcher/utilities/email_listener/__init__.py
@@ -388,15 +388,16 @@ class EmailListener:
         # Start idling
         self.server.idle()
         print("Connection is now in IDLE mode.")
+        print(f"GET TIME : {get_time()}")
         # Set idle timeout to 15 minutes
-        inner_timeout = get_time() + 60
+        inner_timeout = get_time() + 180
         # Until idle times out
         # while True:
         while (get_time() < inner_timeout):
             # Check for a new response every 1 seconds
             try:
                 responses = self.server.idle_check(timeout=1)
-                # print("Server sent:", responses if responses else "nothing")
+                print("Server sent:", responses if responses else "nothing")
                 # If there is a response
                 if (responses):
                     # Suspend the idling
@@ -420,6 +421,7 @@ class EmailListener:
                             zenith_parser.save_data(attachment_list)
                             print('files:', attachment_list)
                     except Exception as e:
+                        self.server.idle()
                         raise e
                     finally:
                         # Restart idling

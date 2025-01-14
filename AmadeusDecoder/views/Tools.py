9d71ac4b17468da15fdc447060a8ea19b96332aa
alain-nambi <mihaja@phidia.onmicrosoft.com>
2025-01-14 11:34:09 +0300

    comment sending email,ajout lien vers testodoo

--- AmadeusDecoder/views/Tools.py
+++ AmadeusDecoder/views/Tools.py
@@ -111,7 +111,7 @@ def call_product_import(request):
     
     status = ''
     try:
-        directory = '/opt/odoo/issoufali-addons/sync_products/data/exported/'
+        directory = '/opt/issoufali/odoo/issoufali-addons/sync_products/data/exported/'
         for file in os.listdir(directory):
             if os.path.isfile(os.path.join(directory, file)):
                 ProductParser.import_product(os.path.join(directory, file), directory)
@@ -127,7 +127,7 @@ def call_customer_import(request):
     
     status = ''
     try:
-        directory = '/opt/odoo/issoufali-addons/export_contacts/data/exported/'
+        directory = '/opt/issoufali/odoo/issoufali-addons/export_contacts/data/exported/'
         for file in os.listdir(directory):
             if os.path.isfile(os.path.join(directory, file)):
                 CustomerParser.import_customer(os.path.join(directory, file), directory)
